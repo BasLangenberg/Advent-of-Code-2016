eb0dcd230c3124220c88672b0bfe7a2401ee599d
Bas Langenberg <baslangenberg@gmail.com>
2016-12-04 22:42:13 +0100

    Add solution for part 1, wip for part 2

---
+++ day004/find_north_pole_object.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    x = ord('a')
+    x += 1
+    print(chr(x))
--- day004/remove_decoy.py
+++ day004/remove_decoy.py
@@ -1,37 +1,40 @@
+from collections import OrderedDict
 import re
-#from pudb import set_trace; set_trace()
 
 def is_real(line):
-    is_real = True
-    for segment in line:
-        print(type(line))
-        if '[' in segment:
-            validation = re.findall(r'\[(.*?)\]',segment)
-            for validator  in validation:
-                for character in validator:
-                    count = 0
-                    segment_check = True
-                    while count < line.__len__() - 2 and segment_check == True:
-                        if character not in line[count]:
-                            is_real = False
-                        else:
-                            segment_check = True
-                        count += 1
+    validation = re.findall(r'\[(.*?)\]',line[-1])
+    encrypted_string = ''
+    linecount = 0
+    while linecount < line.__len__() - 1:
+        encrypted_string += line[linecount]
+        linecount += 1
 
-    if is_real == True:
-        return True
+    all_words = list({(char, encrypted_string.count(char)) for char in encrypted_string})
+    all_words.sort(key=lambda x: (-x[1], x[0]))
+
+    calculated_checksum = "".join([byte for byte, count in all_words])
+
+    if calculated_checksum[:5] == str(validation[0]):
+        # return int(line[-1].strip('['))
+        return int(str(line[-1]).split('[')[0])
     else:
-        return False
+        return 0
+
+# 5342
+# 375110
+# 139587
+# 245102
 
 if __name__ == '__main__':
 
     real = []
+    count_total = 0
 
     with open('input_data_part_one.txt', 'r') as file:
         for line in file:
             line = line.strip('\n')
             data = line.split('-')
-            if is_real(data):
-                real.append(line)
+            count_total += is_real(data)
 
-    print(real)
+    print(count_total)
+
