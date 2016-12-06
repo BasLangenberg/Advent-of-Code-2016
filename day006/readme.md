2e21f54f688604daa134e274634f02a34436367c
Bas Langenberg <baslangenberg@gmail.com>
2016-12-06 21:32:53 +0100

    Add solution for day 6 (both parts)

---
+++ day006/readme.md
@@ -0,0 +1,1 @@
+[Advent of code 2016, day 5](http://adventofcode.com/2016/day/5)
---
+++ day006/signal_and_noise_least.py
@@ -0,0 +1,40 @@
+def decrypt_column(input):
+    width = input[0].__len__()
+    hash = {}
+    decrypt = ''
+    for num in range(width):
+        hash[num] = ''
+
+    for string in input:
+        for character in range(string.__len__()):
+            hash[character] = hash[character] + string[character]
+
+    for set in hash:
+        count = 1000
+        winner = ''
+        for char in hash[set]:
+            if hash[set].count(char) < count:
+                winner, count = char, hash[set].count(char)
+
+        decrypt += winner
+
+    return decrypt
+
+def decrypt():
+    input = []
+    with open('input.txt', 'r') as file:
+        for line in file.readlines():
+            input.append(line.strip('\n'))
+
+    print(decrypt_column(input))
+
+def test_decrypt():
+    input = []
+    with open('test_input.txt', 'r') as file:
+        for line in file.readlines():
+            input.append(line.strip('\n'))
+
+    assert decrypt_column(input) == 'advent'
+
+if __name__ == '__main__':
+    decrypt()
